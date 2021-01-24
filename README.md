b7b13859078def6a23f2c313981f80c98ca5717c
jessev14 <jessev14@users.noreply.github.com>
2021-01-24 16:18:58 -0500

    v1.0.0
    Initial release.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# reset-movement
- Reset Movement module for FVTT
+# Reset Movement
+
+Reset Movement (RM) is a FoundryVTT module that allows GM users to reset the current combatant's position to their position at the start of their turn.
+
+## Usage
+
+The current combatant's starting position is stored as flags on the token at the start of their token's turn.
+
+GM users can click a button on the Token Layer toolbar to reset the current combatant to the saved position.
--- scripts/RMTool.js
+++ scripts/RMTool.js
@@ -1,5 +1,7 @@
 export function initRMTool() {
     Hooks.on("getSceneControlButtons", (controls) => {
+        if (!game.user.isGM) return;
+
         const bar = controls.find(c => c.name === "token");
         bar.tools.push({
             name: "RM Tool",
@@ -13,7 +15,7 @@ export function initRMTool() {
 
 async function resetMovement() {
     const currentToken = canvas.tokens.get(game.combat.combatant.token._id);
-    //await currentToken.setPosition(currentToken.getFlag("reset-movement", "x"), currentToken.getFlag("reset-movement", "y"));
+    await currentToken.setPosition(currentToken.getFlag("reset-movement", "x"), currentToken.getFlag("reset-movement", "y"));
     await currentToken.update({
         x: currentToken.getFlag("reset-movement", "x"),
         y: currentToken.getFlag("reset-movement", "y")

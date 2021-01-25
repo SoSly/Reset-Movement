3c20e98de03f38dba0528957c87e8c3813b56b04
jessev14 <jessev14@users.noreply.github.com>
2021-01-25 10:40:41 -0500

    Update RMTool.js
    change tool icon

--- scripts/RMTool.js
+++ scripts/RMTool.js
@@ -6,7 +6,7 @@ export function initRMTool() {
         bar.tools.push({
             name: "RM Tool",
             title: "Reset Movemnt",
-            icon: "fas fa-shoe-prints",
+            icon: "fas fa-walking",
             onClick: () => resetMovement(),
             button: true
         });

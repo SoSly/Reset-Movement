bd38476ae9638241c1184400282e93e225dbe25b
Glasses_Make_Me_Smart <48263319+kgruszcz@users.noreply.github.com>
2022-01-10 18:23:59 -0800

    Update resetMovement.js

--- scripts/resetMovement.js
+++ scripts/resetMovement.js
@@ -11,23 +11,32 @@ Hooks.once("init", () => {
         type: Boolean,
         default: true
     });
+    
+    game.settings.register('reset-movement', 'gmOnly', {
+        name: game.i18n.localize("reset-movement.settings.gmOnly"),
+        hint: "",
+        scope: "world",
+        config: true,
+        type: Boolean,
+        default: true
+    });
 
     initRMTool();
 });
 
 Hooks.once("ready", () => {
     Hooks.on("updateCombat", async (combat, changed, options, userId) => {
-        if (!game.user.isGM || !combat.combatant) return;
-        const currentToken = canvas.tokens.get(combat.combatant.token._id);
-        await currentToken.setFlag("reset-movement", "startPosition", { x: currentToken.x, y: currentToken.y, rotation: currentToken.data.rotation });
-        await currentToken.setFlag("reset-movement", "positionHistory", [currentToken.getFlag("reset-movement", "startPosition")]);
+        if ((game.settings.get("reset-movement", "gmOnly") && !game.user.isGM) || !combat.combatant) return;
+        const currentToken = canvas.tokens.get(combat.combatant.token.id);
+        await currentToken.document.setFlag("reset-movement", "startPosition", { x: currentToken.x, y: currentToken.y, rotation: currentToken.rotation });
+        await currentToken.document.setFlag("reset-movement", "positionHistory", [currentToken.getFlag("reset-movement", "startPosition")]);
     });
 
     Hooks.on("deleteCombat", async () => {
-        if (!game.user.isGM) return;
+        if (game.settings.get("reset-movement", "gmOnly") && !game.user.isGM) return;
         for (let tkn of canvas.tokens.placeables) {
-            await tkn.unsetFlag("reset-movement", "startPosition");
-            await tkn.unsetFlag("reset-movement", "positionHistory");
+            await tkn.document.unsetFlag("reset-movement", "startPosition");
+            await tkn.document.unsetFlag("reset-movement", "positionHistory");
         }
     });
 })

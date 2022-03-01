9efcfc4b5fa05ebca1f8736cec3bfe911792a05f
Jesse Vo <68755874+jessev14@users.noreply.github.com>
2022-03-01 15:39:36 -0500

    Update resetMovement.js

--- scripts/resetMovement.js
+++ scripts/resetMovement.js
@@ -28,15 +28,19 @@ Hooks.once("ready", () => {
     Hooks.on("updateCombat", async (combat, changed, options, userId) => {
         if ((game.settings.get("reset-movement", "gmOnly") && !game.user.isGM) || !combat.combatant) return;
         const currentToken = canvas.tokens.get(combat.combatant.token.id);
-        await currentToken.document.setFlag("reset-movement", "startPosition", { x: currentToken.x, y: currentToken.y, rotation: currentToken.rotation });
-        await currentToken.document.setFlag("reset-movement", "positionHistory", [currentToken.document.getFlag("reset-movement", "startPosition")]);
+        if (game.user.id === game.users.find(u => u.active && u.isGM)?.id) {
+            await currentToken.document.setFlag("reset-movement", "startPosition", { x: currentToken.x, y: currentToken.y, rotation: currentToken.rotation });
+            await currentToken.document.setFlag("reset-movement", "positionHistory", [currentToken.document.getFlag("reset-movement", "startPosition")]);
+        }
     });
 
     Hooks.on("deleteCombat", async () => {
         if (game.settings.get("reset-movement", "gmOnly") && !game.user.isGM) return;
         for (let tkn of canvas.tokens.placeables) {
-            await tkn.document.unsetFlag("reset-movement", "startPosition");
-            await tkn.document.unsetFlag("reset-movement", "positionHistory");
+            if (game.user.id === game.users.find(u => u.active && u.isGM)?.id) {
+                await tkn.document.unsetFlag("reset-movement", "startPosition");
+                await tkn.document.unsetFlag("reset-movement", "positionHistory");
+            }
         }
     });
 })
